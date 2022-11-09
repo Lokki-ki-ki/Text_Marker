15072560de4daf29b6157d8ae8347e234240a6fd
132468756 <70301065+132468756@users.noreply.github.com>
2022-11-09 11:29:35 +0800

    Update dataeng and feature engineering

--- dataeng.py
+++ dataeng.py
@@ -8,22 +8,18 @@ from spellchecker import SpellChecker
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.sentiment import SentimentIntensityAnalyzer
+import textstat
+from textstat.textstat import textstatistics
 from textacy import preprocessing
 import spacy
 import collections
+from lexrank import STOPWORDS, LexRank
 from path import Path
 import gensim
 from gensim.utils import simple_preprocess
 from nltk.corpus import stopwords
 import gensim.corpora as corpora
 from gensim.models import CoherenceModel
-import re
-import pickle
-import sys
-sys.path.append("/opt/anaconda3/lib/python3.9/site-packages/")
-from lexrank import STOPWORDS, LexRank
-import textstat
-from textstat.textstat import textstatistics
 
 
 class DataEng:
@@ -198,7 +194,7 @@ class DataEng:
         lxr = LexRank(self.documents, stopwords=STOPWORDS['en'])
         ranks = lxr.rank_sentences(self.sent, threshold=None)
 
-        return sum(ranks)/len(ranks)-min(ranks), np.quantile(ranks,[0.25,0.75])[1] - np.quantile(ranks,[0.25,0.75])[1]
+        return sum(ranks)/len(ranks)-min(ranks), np.quantile(ranks, [0.25, 0.75])[1] - np.quantile(ranks, [0.25, 0.75])[1]
 
     def sentence_complexity(self):
         sentences = self.sent
@@ -336,17 +332,3 @@ class DataEng:
             num_of_distinct_adj,
             num_of_distinct_adv
         ]
-
-
-if __name__ == "__main__":
-    # Used for testing
-    input = "I think that students would benefit from learning at home,because they wont have to change and get up early in the morning to shower and do there hair. taking only classes helps them because at there house they'll be pay more attention. they will be comfortable at home.The hardest part of school is getting ready. you wake up go brush your teeth and go to your closet and look at your cloths. after you think you picked a outfit u go look in the mirror and youll either not like it or you look and see a stain. Then you'll have to change. with the online classes you can wear anything and stay home and you wont need to stress about what to wear.most students usually take showers before school. they either take it before they sleep or when they wake up. some students do both to smell good. that causes them do miss the bus and effects on there lesson time cause they come late to school. when u have online classes u wont need to miss lessons cause you can get everything set up and go take a shower and when u get out your ready to go.when your home your comfortable and you pay attention. it gives then an advantage to be smarter and even pass there classmates on class work. public schools are difficult even if you try. some teacher dont know how to teach it in then way that students understand it. that causes students to fail and they may repeat the class."
-    data = DataEng(input).Engineering()
-    data.to_csv("test.csv", index=False)
-    # print(result)
-# print(len(nltk.sent_tokenize(input)))
-
-# split() -> punctuation with word
-# nltk -> seperate punctuation
-
-
